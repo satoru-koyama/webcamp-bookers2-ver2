78cb47c704b7d047603567559e2dc9d4f8cf92d1
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-07-02 07:36:03 +0000

    [Add] favoriteモデル, favoritesコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    resources :favorites, only: [:create, :destroy]
+  end
   resources :books
+
 end
