6c70f1c9769638943c6f7d19aa2f8e1d32da376c
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-07-03 01:48:25 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :users, only: [:show,:index,:edit,:update] do
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books do
     resources :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
-  resources :books
 
 end
